da5cdbdad72e8e534c6ce285ceb96ad0fe383559
hoffmanaiden <hoffman.aiden@gmail.com>
2020-07-04 13:54:33 -0400

    took App.js out of gitignore

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+const KEY = 'AIzaSyBWFcOi8NDRzDJhiQjKchDJev2mdXOJ4xE';
+
+class App extends Component {
+  state = {
+    videoList: [],
+    selectedVideo: null
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: 10,
+        key: `${KEY}`
+      }
+    })
+    this.setState({ 
+      videoList: response.data.items,
+      selectedVideo: response.data.items[0]
+     });
+  };
+
+  onVideoSelect = (video) => {
+    // console.log(`Selected video: ${video.snippet.title}`);
+    this.setState({ selectedVideo: video });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onSearchSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              {this.state.selectedVideo !== null ?
+                <VideoDetail video={this.state.selectedVideo} /> :
+                ''
+              }
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videoList} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
