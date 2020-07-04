3568dea688f5b55181e22f8b59c4221857752a25
hoffmanaiden <hoffman.aiden@gmail.com>
2020-07-04 14:43:09 -0400

    learned env vars cant be accessed at runtime by react

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,7 @@ import youtube from '../api/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-// const KEY = 'AIzaSyBWFcOi8NDRzDJhiQjKchDJev2mdXOJ4xE';
-const KEY = process.env.YOUTUBE_API_KEY;
+const KEY = 'AIzaSyBWFcOi8NDRzDJhiQjKchDJev2mdXOJ4xE';
 
 class App extends Component {
   state = {
